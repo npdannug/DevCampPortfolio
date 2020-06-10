f5bd5ee2cc9291f7925596623f4d19ad85d79292
Kerbs <npdannug16@gmail.com>
2020-06-10 16:14:11 +0800

    Added index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+     ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+     voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in
+     voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in
+     voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  )
+end
+
+puts "10 blogs created"
+
+5.times do |skill|
+  Skill.create!(
+  	title: "My Skill #{skill}",
+  	percent_utilized: 15
+
+  )
+end
+
+puts "5 Skills created"
+
+9.times do |p|
+  Portfolio.create!(
+  	title: "My Porfolio #{p}",
+  	subtitle:"My Porfolio subtitle #{p}",
+  	body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+     ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+     voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in
+     voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  	main_image:"https://via.placeholder.com/600x400",
+  	thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+
+puts "9 Porfolios created"
