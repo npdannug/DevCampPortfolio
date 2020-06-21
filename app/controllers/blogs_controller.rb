d449305e09c173404accb66b59c0d4b0a54e18c6
Kerbs <npdannug16@gmail.com>
2020-06-21 17:56:48 +0800

    added helper methods

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,7 +29,7 @@ class BlogsController < ApplicationController
   # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
-
+    @blog.topic_id = 1
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
