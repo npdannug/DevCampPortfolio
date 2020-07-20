5228f03138672da93d27df20f073e6ef184427b9
Kerbs <npdannug16@gmail.com>
2020-07-20 13:38:47 +0800

    image generator adjustment

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -5,7 +5,7 @@ module PortfoliosHelper
 
   def portfolio_item_img item, type
   	if type == 'thumb'
-  	  item.thumb_image.url || image_generator(height: '350', width: '200')
+  	  item.thumb_image.url || image_generator(height: '348', width: '242')
   	else
   	  item.main_image.url || image_generator(height: '600', width: '400')
   	end
