7fe7fa91c9c7f236c52005e2afc7349503b0d704
Kerbs <npdannug16@gmail.com>
2020-07-23 16:55:03 +0800

    added comment form in blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,8 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of
+  validates_presence_of :title, :body
+  belongs_to :topic
 
   has_many :comments, dependent: :destroy
 
