cab92913196113ba4a518c3a2c84904f00451b18
Kerbs <npdannug16@gmail.com>
2020-07-29 17:53:20 +0800

    implemented source helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,10 +22,11 @@ module ApplicationHelper
     end 
   end
 
-  def source_helper(layout_name)
+  def source_helper(styles)
   	if session[:source]
-      greeting = "Thanks for visiting from #{session[:source]} and you are on the #{layout_name} "
-  	  content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting from #{session[:source]}, please feel free to 
+                  #{link_to 'contact me', contact_me_path } if you'd like to work together."
+  	  content_tag(:p, greeting.html_safe, class: styles)
     end 
   end
 
